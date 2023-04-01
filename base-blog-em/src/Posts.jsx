e9278ed3eb5c819811c7ae6c5caef7a0ed19e803
추성준 <91831079+ChuSeongJun@users.noreply.github.com>
2023-04-01 14:12:23 +0900

    16번까지 완료

---
+++ base-blog-em/src/Posts.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
+
+import { PostDetail } from "./PostDetail";
+const maxPostPage = 10;
+
+async function fetchPosts(pageNum) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0${pageNum}`
+  );
+  return response.json();
+}
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClint = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClint.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+    }
+  }, [currentPage, queryClint]);
+
+  // replace with useQuery
+  const { data, isError, error, isLoading, isFetching } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true,
+    }
+  );
+  if (isLoading) return <h3>Loading....</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>에러임 ㅋ</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
+          Previous page
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
